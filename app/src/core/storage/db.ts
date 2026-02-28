1ba5994127e7cf129c2e834229bd279e390c544e
Viktortown <viktortown@gmail.com>
2026-03-01 00:03:36 +0500

    PR-05: missions 1d/3d with core rewards in Goals

--- app/src/core/storage/db.ts
+++ app/src/core/storage/db.ts
@@ -26,7 +26,7 @@ export interface OracleScenarioRecord extends OracleScenario {
   id?: number
 }
 
-export const schemaVersion = 17
+export const schemaVersion = 18
 
 class ConcoreDb extends Dexie {
   checkins!: EntityTable<CheckinRecord, 'id'>
